0be95d8c783ae138a0103d14caa2b517b82a7427
Odyssey <odysseyhyd@gmail.com>
2011-03-14 17:33:15 +0100

    HD 033. Secretos del oficio
    http://www.wowhead.com/spell=57933

--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -4950,6 +4950,18 @@ void AuraEffect::HandleAuraDummy(AuraApplication const* aurApp, uint8 mode, bool
                     if (GetId() == 61777)
                         target->CastSpell(target, GetAmount(), true);
                     break;
+                case SPELLFAMILY_ROGUE:
+                {
+                    switch(GetId())
+                    {
+                        case 59628: // Tricks of the Trade
+                            caster->SetReducedThreatPercent(0, 0);
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                }
                 default:
                     break;
             }
