09f8b47a8433aa7405f4f24076bf13f5c44087cb
Odyssey <odysseyhyd@gmail.com>
2011-07-08 19:34:53 +0200

    HD 075. Quest 13346 & 13350 & 13367 & 13368
    Signed-off-by: Odyssey <odysseyhyd@gmail.com>

---
+++ sql/updates/HD_075_world.sql
@@ -0,0 +1,3 @@
+-- Q13346,13350,13367,13368
+UPDATE item_template SET ScriptName='item_alumeths_remains' WHERE entry=44480;
+UPDATE creature_template SET faction_A=16, faction_H=16 WHERE entry=32300;
