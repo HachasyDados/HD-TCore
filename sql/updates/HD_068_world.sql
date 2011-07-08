f39395b75c1e5f5fc8877444fade8209d442e10d
Karlos <hachasydados@gmail.com>
2011-07-08 19:24:39 +0200

    HD 068. Quest 11670 & 11684 & 11713
    http://www.wowhead.com/?quest=11670
    http://www.wowhead.com/?quest=11684
    http://www.wowhead.com/?quest=11713

---
+++ sql/updates/HD_068_world.sql
@@ -0,0 +1,7 @@
+-- Q11670
+UPDATE creature_template SET KillCredit1=25581 WHERE entry=25430;
+
+-- Q11684 & Q11713
+UPDATE creature SET position_x=3486.13, position_y=4516.12, position_z=-20.7393 WHERE id=25664;
+UPDATE creature SET position_x=3645.46, position_y=4554.54, position_z=-20.74 WHERE id=25665;
+UPDATE creature SET position_x=3650.19, position_y=4743.17, position_z=-20.7399 WHERE id=25666;
