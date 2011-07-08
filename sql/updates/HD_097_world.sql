e0e81eb23f25585c2f7dd73c155c43604303b145
Souler <soulerhyd@gmail.com>
2011-07-08 20:37:39 +0200

    HD 097. Una derrota asombrosa en el circulo (11300)
    Signed-off-by: Souler <soulerhyd@gmail.com>

---
+++ sql/updates/HD_097_world.sql
@@ -0,0 +1,2 @@
+DELETE FROM `smart_scripts` WHERE (`entryorguid` IN (24213, 24215, 24214, 23931) AND `event_type`=6);
+DELETE FROM `smart_scripts` WHERE (`entryorguid` IN (24213, 24215, 24214, 23931) AND `event_type`=50);
