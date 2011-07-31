a7d579bfa42bf7ec664eff38969f4f93ad354017
Odyssey <odysseyhyd@gmail.com>
2011-07-31 16:24:49 +0200

    HD 003. Ajustes en el calculo de indice de arenas tras finalizar cada encuentro

--- src/server/game/Battlegrounds/Battleground.cpp
+++ src/server/game/Battlegrounds/Battleground.cpp
@@ -739,8 +739,8 @@ void Battleground::EndBattleground(uint32 winner)
                 loser_matchmaker_rating = GetArenaMatchmakerRating(GetOtherTeam(winner));
                 winner_team_rating = winner_arena_team->GetRating();
                 winner_matchmaker_rating = GetArenaMatchmakerRating(winner);
-                winner_change = winner_arena_team->WonAgainst(loser_matchmaker_rating);
-                loser_change = loser_arena_team->LostAgainst(winner_matchmaker_rating);
+                winner_change = winner_arena_team->WonAgainst(loser_team_rating);
+                loser_change = loser_arena_team->LostAgainst(winner_team_rating);
                 sLog->outArena("--- Winner rating: %u, Loser rating: %u, Winner MMR: %u, Loser MMR: %u, Winner change: %d, Loser change: %d ---", winner_team_rating, loser_team_rating,
                     winner_matchmaker_rating, loser_matchmaker_rating, winner_change, loser_change);
                 SetArenaTeamRatingChangeForTeam(winner, winner_change);
