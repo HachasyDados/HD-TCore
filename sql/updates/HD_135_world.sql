19d1aa783c1919c1f608cabcaf8e2ffa5861fd98
Odyssey <odysseyhyd@gmail.com>
2011-09-01 17:43:33 +0200

    HD 135. Fix quest 4506 - Corrupted Sabers

---
+++ sql/updates/HD_135_world.sql
@@ -0,0 +1,2 @@
+UPDATE creature_template SET AIName='', ScriptName='npc_q4506_cat' WHERE entry=9937;
+UPDATE creature_template SET scale=3, speed_walk=1.146, speed_run=1.914, faction_A=35, faction_H=35, unit_flags=770, flags_extra=2 WHERE entry=10657;
