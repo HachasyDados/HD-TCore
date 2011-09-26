d5493547019f68954cbce3463150a72f861486ea
Karlos <hachasydados@gmail.com>
2011-09-26 10:35:18 +0200

    HD 132. Scripts/Oculus: Full revision & major rewrite

--- src/server/scripts/Northrend/Nexus/Oculus/boss_varos.cpp
+++ src/server/scripts/Northrend/Nexus/Oculus/boss_varos.cpp
@@ -21,10 +21,14 @@
 
 enum Says
 {
-    SAY_AGGRO           = 0,
-    SAY_AZURE           = 1,
-    SAY_AZURE_EMOTE     = 2,
-    SAY_DEATH           = 3
+    SAY_AGGRO           = -1578029,
+    SAY_AZURE_1         = -1578030,
+    SAY_AZURE_2         = -1578031,
+    SAY_AZURE_3         = -1578032,
+    SAY_KILL_1          = -1578033,
+    SAY_KILL_2          = -1578034,
+    SAY_DEATH           = -1578035,
+    SAY_AZURE_EMOTE     = -1578036,
 };
 
 enum Spells
@@ -38,6 +42,7 @@ enum Spells
     SPELL_CALL_AMPLIFY_MAGIC                      = 51054,
 
     SPELL_ICE_BEAM                                = 49549,
+    SPELL_ARCANE_BEAM_VISUAL                      = 51024,
     SPELL_ARCANE_BEAM_PERIODIC                    = 51019,
     SPELL_SUMMON_ARCANE_BEAM                      = 51017
 };
@@ -85,7 +90,7 @@ public:
         {
             _EnterCombat();
 
-            Talk(SAY_AGGRO);
+            DoScriptText(SAY_AGGRO, me);
         }
 
         float GetCoreEnergizeOrientation()
@@ -127,8 +132,8 @@ public:
                     case EVENT_CALL_AZURE:
                         // not sure how blizz handles this, i cant see any pattern between the differnt spells
                         DoCast(me, SPELL_CALL_AZURE_RING_CAPTAIN);
-                        Talk(SAY_AZURE);
-                        Talk(SAY_AZURE_EMOTE);
+                        DoScriptText(RAND(SAY_AZURE_1, SAY_AZURE_2, SAY_AZURE_3), me);
+                        DoScriptText(SAY_AZURE_EMOTE, me);
                         events.ScheduleEvent(EVENT_CALL_AZURE, urand(20, 25) * IN_MILLISECONDS);
                         break;
                     case EVENT_AMPLIFY_MAGIC:
@@ -145,8 +150,14 @@ public:
         {
             _JustDied();
 
-            Talk(SAY_DEATH);
+            DoScriptText(SAY_DEATH, me);
         }
+
+        void KilledUnit(Unit* /*victim*/)
+        {
+            DoScriptText(RAND(SAY_KILL_1, SAY_KILL_2), me);
+        }
+
     private:
         bool firstCoreEnergize;
         float coreEnergizeOrientation;
@@ -176,10 +187,7 @@ class npc_azure_ring_captain : public CreatureScript
             void SpellHitTarget(Unit* target, SpellInfo const* spell)
             {
                 if (spell->Id == SPELL_ICE_BEAM)
-                {
-                    target->CastSpell(target, SPELL_SUMMON_ARCANE_BEAM, true);
-                    me->DespawnOrUnsummon();
-                }
+                    DoAction(ACTION_ARCANE_BEAM);
             }
 
             void UpdateAI(const uint32 /*diff*/)
@@ -215,12 +223,32 @@ class npc_azure_ring_captain : public CreatureScript
                                 {
                                     me->SetReactState(REACT_PASSIVE);
                                     me->RemoveUnitMovementFlag(MOVEMENTFLAG_WALKING);
-                                    me->GetMotionMaster()->MovePoint(ACTION_CALL_DRAGON_EVENT, victim->GetPositionX(), victim->GetPositionY(), victim->GetPositionZ() + 20.0f);
+                                    me->GetMotionMaster()->MovePoint(ACTION_CALL_DRAGON_EVENT, victim->GetPositionX(), victim->GetPositionY(), victim->GetPositionZ() + 12.0f);
                                     targetGUID = victim->GetGUID();
                                 }
                             }
                         }
                         break;
+                   case ACTION_ARCANE_BEAM:
+                       if (instance)
+                       {
+                           if (Unit* victim = me->GetPlayer(*me, targetGUID))
+                           {
+                                if(Creature* beamTrigger = me->SummonCreature(28239, victim->GetPositionX() + urand(0,5), victim->GetPositionY() + urand(0,5), victim->GetPositionZ()))
+                                {
+                                    beamTrigger->RemoveAllAuras();
+                                    beamTrigger->setFaction(me->getFaction());
+                                    beamTrigger->AddAura(SPELL_ARCANE_BEAM_PERIODIC, beamTrigger);
+                                    beamTrigger->SetSpeed(MOVE_RUN, 0.95f, true);
+                                    me->CastSpell(beamTrigger, SPELL_ARCANE_BEAM_VISUAL, true);
+                                    beamTrigger->Attack(victim, false);
+                                    beamTrigger->GetMotionMaster()->MoveChase(victim, 1.7f);
+                                    uint32 timer = urand(15000, 20000);
+                                    beamTrigger->DespawnOrUnsummon(timer);
+                                    me->DespawnOrUnsummon(timer+1000);
+                                }
+                           }
+                       }
                 }
            }
 
--- src/server/scripts/Northrend/Nexus/Oculus/oculus.h
+++ src/server/scripts/Northrend/Nexus/Oculus/oculus.h
@@ -32,7 +32,10 @@ enum Data64
     DATA_DRAKOS,
     DATA_VAROS,
     DATA_UROM,
-    DATA_EREGOS
+    DATA_EREGOS,
+
+    DATA_DRAKE_IN_USE,
+    DATA_DRAKE_NOT_IN_USE,
 };
 
 enum Bosses
@@ -48,9 +51,10 @@ enum Bosses
 
 enum GameObjects
 {
-    GO_DRAGON_CAGE_DOOR                           = 193995,
-    GO_EREGOS_CACHE_N                             = 191349,
-    GO_EREGOS_CACHE_H                             = 193603
+    GO_DRAGON_CAGE_DOOR   = 193995,
+    GO_EREGOS_CACHE_N     = 191349,
+    GO_EREGOS_CACHE_H     = 193603,
+    GO_CACHE_SPOTLIGHT    = 191351,
 };
 
 enum SpellEvents
@@ -60,7 +64,8 @@ enum SpellEvents
 
 enum CreatureActions
 {
-    ACTION_CALL_DRAGON_EVENT = 1
+    ACTION_CALL_DRAGON_EVENT = 1,
+    ACTION_ARCANE_BEAM,
 };
 
 enum OculusWorldStates
@@ -72,5 +77,12 @@ enum OculusWorldStates
 enum OculusSpells
 {
     SPELL_CENTRIFUGE_SHIELD = 50053
+};
+
+enum VehicleDrakes
+{
+    NPC_RUBY_DRAKE_VEHICLE    = 27756,
+    NPC_AMBER_DRAKE_VEHICLE   = 27755,
+    NPC_EMERALD_DRAKE_VEHICLE = 27692,
 };
 #endif
