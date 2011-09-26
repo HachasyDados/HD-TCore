1ad559fc897fcf208512593dade285756253c7e3
Karlos <hachasydados@gmail.com>
2011-09-26 10:02:37 +0200

    HD 004. Spells 69672 y 69673: Efecto y dependencia de genero.

---
+++ sql/updates/HD_004_world.sql
@@ -0,0 +1,4 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (69672,69673);
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
+(69672,'spell_sunreaver_disguise'),
+(69673,'spell_silvert_convenant_disguise');
