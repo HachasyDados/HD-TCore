fb7044c41a719347d192148da0090e0015f3b3ab
Karlos <hachasydados@gmail.com>
2011-09-30 17:02:28 +0200

    HD 148. Correct Dual spec cost for NPCs 3033, 3041, 3042, 3043.
    Signed-off-by: Karlos <hachasydados@gmail.com>

---
+++ sql/updates/HD_148_world.sql
@@ -0,0 +1,4 @@
+UPDATE gossip_menu_option SET box_money='10000000', box_text='Are you sure you wish to purchase a Dual Talent Specialization?' WHERE menu_id='3921' AND id='2';# npc 3033
+UPDATE gossip_menu_option SET box_money='10000000', box_text='Are you sure you wish to purchase a Dual Talent Specialization?' WHERE menu_id='4527' AND id='2';# npc 3041
+UPDATE gossip_menu_option SET box_money='10000000', box_text='Are you sure you wish to purchase a Dual Talent Specialization?' WHERE menu_id='4525' AND id='2';# npc 3042
+UPDATE gossip_menu_option SET box_money='10000000', box_text='Are you sure you wish to purchase a Dual Talent Specialization?' WHERE menu_id='4526' AND id='2';# npc 3043
