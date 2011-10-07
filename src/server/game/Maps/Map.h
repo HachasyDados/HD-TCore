4ead6b440c81ca6e649389a997f86a815f531fd7
Odyssey <odysseyhyd@gmail.com>
2011-10-08 01:12:46 +0200

    HD 157. Basic Implementation of Dynamic LoS for GameObjects Original patch by kerhong.
    Some modifications by souler, listed below.
    
    Cambios menores el el sistema de DynLoS para añadir coordenada de altura
    
    Eliminado de volcado a logs de datos para el desarrollo.
    Totalmente inecesarios, solo eran para enterarme yo.
    
    Más limpieza de codigo y añadadias apreciaciones sobre el sistema añadido para la coordenada Z
    
    Signed-off-by: Odyssey <odysseyhyd@gmail.com>

--- src/server/game/Maps/Map.h
+++ src/server/game/Maps/Map.h
@@ -200,6 +200,30 @@ public:
 #pragma pack(push, 1)
 #endif
 
+class DynamicLOSObject
+{
+    public:
+        DynamicLOSObject();
+        bool IsBetween(float x, float y, float z, float x2, float y2, float z2);
+        bool IsInside(float x, float y);
+        bool IsOverOrUnder(float z);
+        float GetDistance(float x, float y);
+        bool IsActive();
+        void SetActiveState(bool state);
+        void SetCoordinates(float x, float y);
+        void SetZ(float z);
+        void SetRadius(float r);
+        void SetHeight(float h);
+        bool HasHeightInfo();
+    private:
+        float _x;
+        float _y;
+        float _z;
+        float _height;
+        float _radius;
+        bool _active;
+};
+
 struct InstanceTemplate
 {
     uint32 Parent;
@@ -424,6 +448,21 @@ class Map : public GridRefManager<NGridType>
 
         InstanceMap* ToInstanceMap(){ if (IsDungeon())  return reinterpret_cast<InstanceMap*>(this); else return NULL;  }
         const InstanceMap* ToInstanceMap() const { if (IsDungeon())  return (const InstanceMap*)((InstanceMap*)this); else return NULL;  }
+    /*
+     **********************
+     * DYNAMIC LOS SYSTEM *
+     **********************
+    */
+    public:
+        uint32 AddDynLOSObject(float x, float y, float radius);
+        uint32 AddDynLOSObject(float x, float y, float z, float radius, float height);
+        void SetDynLOSObjectState(uint32 id, bool state);
+        bool GetDynLOSObjectState(uint32 id);
+        bool IsInDynLOS(float x, float y, float z, float x2, float y2, float z2);
+    private:
+        std::map<uint32, DynamicLOSObject*> m_dynamicLOSObjects;
+        uint32 m_dynamicLOSCounter;
+    /* END */
     private:
         void LoadMapAndVMap(int gx, int gy);
         void LoadVMap(int gx, int gy);
