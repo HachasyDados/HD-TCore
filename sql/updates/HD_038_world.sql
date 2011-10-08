2548cd0a89cae60c9281cd2e19776c93d0c8524c
Karlos <hachasydados@gmail.com>
2011-10-08 13:58:56 +0200

    HD 038. Quest 12983 - La ultima de su raza

---
+++ sql/updates/HD_038_world.sql
@@ -0,0 +1,3 @@
+-- Q 12983
+UPDATE creature_template SET AIName='', ScriptName='npc_injured_icemaw' WHERE entry=29563;
+UPDATE creature_template SET AIName='', ScriptName='npc_harnessed_icemaw' WHERE entry=30468;
