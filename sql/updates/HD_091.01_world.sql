96ce852c11d4c4819a3a5500ac23196b8a862938
Souler <soulerhyd@gmail.com>
2011-10-09 19:37:12 +0200

    HD 91.01 Core: Show AntiCheat and Warden warnings in the GMs chat log.

---
+++ sql/updates/HD_091.01_world.sql
@@ -0,0 +1,3 @@
+DELETE FROM `trinity_string` WHERE `entry` = 11002;
+INSERT INTO `trinity_string` (`entry`, `content_default`) VALUES
+(11002, '|cFFFFBF00[%s]:|cFFFFFFFF [%s] |cFF00FFFFdetected as possible cheater.');
